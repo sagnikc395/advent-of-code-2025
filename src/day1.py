f7f9bf346aa5533f6a8d0faf00a4d19b009ed3fc
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-12-01 20:50:55 -0500

    feat: solved day1 part2

--- src/day1.py
+++ src/day1.py
@@ -1,5 +1,5 @@
 import os 
-
+import math 
 
 def solve_part1(contents):
     current_position = 50 
@@ -23,8 +23,34 @@ def solve_part1(contents):
     
     return zero_count
 
+def calculate_zero_crossings(start_pos,direction,distance):
+    if direction == 'R':
+        crossings = math.floor((start_pos + distance) / 100)
+    elif direction == 'L':
+        crossings = math.floor((start_pos -1) / 100) - math.floor((start_pos - distance - 1) / 100)
+    else:
+        return 0
+    
+    return crossings
+
 def solve_part2(contents):
-    pass 
+    current_position = 50 
+    total_zero_crossings = 0 
+
+    for position in contents:
+        direction = position[0]
+        distance = int(position[1:])
+
+        crossings_this_rotation = calculate_zero_crossings(current_position,direction,distance)
+        total_zero_crossings += crossings_this_rotation
+
+        if direction == 'L':
+            current_position = (current_position - distance) % 100
+        elif direction == 'R':
+            current_position = (current_position + distance) % 100
+
+    return total_zero_crossings
+         
 
 
 def main():
@@ -33,7 +59,9 @@ def main():
     with open(data_path) as f:
         contents = [i for i in f.read().strip().split("\n")]
     
-    print(solve_part1(contents))
+    print(f"Part1 solution: {solve_part1(contents)}")
+    print(f"Part2 solution: {solve_part2(contents)}")
+    
 
 
 
