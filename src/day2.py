093d7b83eb1d48cd2e63305f5ac76a374c1e92be
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-12-03 00:04:58 -0500

    feat: solved day2

--- src/day2.py
+++ src/day2.py
@@ -1,4 +1,5 @@
 import re
+from typing import SupportsAbs
 
 def check_invalid_id_pattern(number_string):
     pattern = r"^(\d+)\1$"
@@ -21,11 +22,42 @@ def solve_part1(id_ranges):
 
     return summ
 
+def check_invalid_id_pattern2(number_string):
+    length = len(number_string)
+
+    # iterate through possible lengths for the repeating subsequence
+    for l_s in range(1,length // 2 + 1):
+        if length % l_s == 0:
+            subsequence = number_string[:l_s]
+            k = length // l_s
+
+            # check if the original string is exaclty 'subsequence' repeated
+            if subsequence * k == number_string:
+                return True
+    return False
+
+
+def solve_part2(id_ranges):
+    summ = 0
+    for item in id_ranges:
+        ids = item.split('-')
+        firstId = int(ids[0])
+        lastId = int(ids[1])
+
+        for i in range(firstId,lastId+1):
+            if check_invalid_id_pattern2(str(i)):
+                summ += i
+
+    return summ 
+
+
 def main():
     with open('../data/day2-input.txt','r') as f:
         content = f.read().strip().split(",")
 
-    print(solve_part1(content))
+    print(f"Part1 solution: {solve_part1(content)}")
+
+    print(f"Part2 solution: {solve_part2(content)}")
     
         
         
