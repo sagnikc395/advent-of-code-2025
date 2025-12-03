8c1e99e8e45f9ab27483e2c5bf1c55db803cbb10
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-12-03 11:36:10 -0500

    todo: day3

---
+++ src/day3.py
@@ -0,0 +1,15 @@
+def solve_part1(jolts_readings):
+    total_output_joltage = 0
+
+    for joltage in jolts_readings:
+        pass
+
+
+def main():
+    with open("../data/day3-sample.txt",'r') as f:
+        p = f.read().strip().split('\n')
+
+    print(p)
+
+if __name__ == '__main__':
+    main()
